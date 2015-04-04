4e12f683f64d5fb6f7481535a43fcab1a93f87ce
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-04-04 13:10:02 +0530

    Add input field

--- public/assets/js/src/app.js
+++ public/assets/js/src/app.js
@@ -1,5 +1,12 @@
 var app = angular.module('myApp', []);
 
 app.controller('MainCtrl', function($scope) {
+    $scope.data = {};
+    $scope.is_submitted = false;
+
+    $scope.find_links = function() {
+       $scope.is_submitted = true;
+    };
+
     $scope.message = 'hello AngularJS';
 });
