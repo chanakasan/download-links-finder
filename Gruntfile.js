0579d5ec7c0cc6c02fee4c7ee14559d1ce867c40
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-04-04 12:10:53 +0530

    Create Sassy CSS

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+    var config = {};
+    config.sass = {
+        dist: {
+            options: {
+                loadPath: ['node_modules/csstyle/']
+            },
+            files: [{
+                expand: true,
+                cwd: 'resources/stylesheets/',
+                src: ['*.scss'],
+                dest: 'public/assets/css/compiled/',
+                ext: '.css'
+            }]
+        },
+    };
+    config.watch = {
+        css: {
+            files: 'resources/stylesheets/*.scss',
+            tasks: ['sass'],
+            options: {
+                livereload: true,
+            },
+        },
+    };
+
+    grunt.initConfig(config);
+
+    // Load the plugins
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    // Default task(s).
+    grunt.registerTask('default', ['sass', 'watch']);
+};
