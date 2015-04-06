72b4ae24ff4147f6ffc62c9b57d6f2f271475bfb
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-04-06 23:10:45 +0530

    Scrape Hacker News links to articls

--- app.js
+++ app.js
@@ -10,11 +10,37 @@ http = Promise.promisifyAll(http);
 
 app.get('/example', function(request, response) {
 
-    http.getAsync('https://news.ycombinator.com').then(function(html) {
-        console.log('html:');
-        console.log(html.length);
+    http.getAsync('https://news.ycombinator.com').spread(function(raw_response, html) {
+        var collection = [];
+        var $ = cheerio.load(html);
 
-        response.json(html);
+        console.log('title:');
+        console.log($('title').text());
+
+        $('span.sitebit.comhead').each(function(i, element){
+            var a = $(this).prev();
+            var rank = a.parent().parent().text();
+            var title = a.text();
+            var url = a.attr('href');
+            var subtext = a.parent().parent().next().children('.subtext').children();
+            var points = $(subtext).eq(0).text();
+            var username = $(subtext).eq(1).text();
+            var comments = $(subtext).eq(2).text();
+
+            // Our parsed meta data object
+            var metadata = {
+                rank: parseInt(rank),
+                title: title,
+                url: url,
+                points: parseInt(points),
+                username: username,
+                comments: parseInt(comments)
+            };
+
+            collection.push(metadata);
+        });
+
+        response.json(collection);
     }).error(function(err) {
         console.log(err);
         response.sendStatus(400);
