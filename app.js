01f698cfa288522a5dda6bff88818298fe368c48
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-04-06 13:24:42 +0530

    Create route /example

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 var express = require('express');
+var Promise = require('bluebird');
+var http = require('request');
+var cheerio = require('cheerio');
 
 var app = express();
 app.use(express.static('public'));
+
+http = Promise.promisifyAll(http);
+
+app.get('/example', function(request, response) {
+
+    http.getAsync('https://news.ycombinator.com').then(function(html) {
+        console.log('html:');
+        console.log(html.length);
+
+        response.json(html);
+    }).error(function(err) {
+        console.log(err);
+        response.sendStatus(400);
+    });
+
+});
 
 module.exports = app;
