314ce54b55186dde64317be09a06c2ef8cb6b630
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-04-17 07:21:37 +0530

    Fix port binding for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('./app');
 
-var server_port = 3000;
+var server_port = process.env.PORT || 3000;
 app.listen(server_port, function() {
    console.log('Listening on localhost:'+server_port);
 
